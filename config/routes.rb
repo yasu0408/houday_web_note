bc257c1b5f85162b60b58ea6adb3094c70f65b96
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-12-01 11:29:57 +0900

    ゲストユーザーの変更・削除ができないよう設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#index"
-  devise_for :users
+  devise_for :users, controllers: {
+                       registrations: "users/registrations",
+                       passwords: "users/passwords",
+                     }
   # post "/homes/guest_sign_in", to: "homes#new_guest"
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#new_guest"
