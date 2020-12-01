6d9c6fcf6b51bc00881d004a0c7c8e43b1cac118
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-12-01 11:48:34 +0900

    コメントアウトなどの無駄な記述の削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
                        registrations: "users/registrations",
                        passwords: "users/passwords",
                      }
-  # post "/homes/guest_sign_in", to: "homes#new_guest"
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#new_guest"
   end
