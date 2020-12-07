8c12362ba4d5655523b423689af16ce7a469c8c2
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-12-07 11:19:12 +0900

    Postモデルの作成、連絡帳の作成は１日に１回であるバリデーションを追加

---
+++ app/models/note.rb
@@ -0,0 +1,13 @@
+class Note < ApplicationRecord
+  belongs_to :user
+  CONDITION = ["よい", "ふつう", "わるい"]
+  HUMOR = ["よい", "ふつう", "わるい"]
+  validates :condition, presence: true, inclusion: { in: CONDITION }
+  validates :humor, presence: true, inclusion: { in: HUMOR }
+  validates :temp, presence: true
+
+  validates :user_id, uniqueness: {
+                        scope: :date,
+                        message: "さんは連絡帳を同日に１つしか作ることができません",
+                      }
+end
