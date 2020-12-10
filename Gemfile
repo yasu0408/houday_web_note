a2b7d48a72de9d786c223ea48fac84f6567901f7
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-12-10 16:16:51 +0900

    バルクインサートにactiverecored-importを利用

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'rails-i18n', '~> 6.0'
 gem 'devise-i18n'
 gem 'devise-bootstrap-views', '~> 1.0'
 gem 'gon'
-
+gem 'activerecord-import'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,30 +1,26 @@
 class CalendarsController < ApplicationController
-  # before_action :set_calendar, only: %i[edit update]
-
   def edit
     gon.selected_date = current_user.calendars.pluck(:date) if current_user.calendars.present?
-    # 30日以内を取得するコードをここに書く
-
   end
 
   def update
     submitted_dates = params[:calendar][:date].split(" ").map(&:to_date)
     save_dates = submitted_dates - current_user.calendars.pluck(:date)
-    save_dates.each do |save_date|
-      current_user.calendars.create!(date: save_date)
-      # binding.pry
+    list = save_dates.map do |date|
+      { user_id: current_user.id, date: date }
     end
+    # list = []
+    # save_dates.each do |save_date|
+    #   hash = { user_id: current_user.id, date: save_date }
+    #   list << hash
+    Calendar.import!(list)
+
+    # save_dates.each do |save_date|
+    #   current_user.calendars.create!(date: save_date)
+    # end
+
     delete_dates = current_user.calendars.pluck(:date) - submitted_dates
-    current_user.calendars.where(date: delete_dates).destroy_all
+    current_user.calendars.where(date: delete_dates).delete_all
     redirect_to root_path
   end
-
-  # private
-
-  # def set_calendar
-  #   date = Date.current
-  #   range = date..date.next_month
-  #   @calendar = current_user.calendars.where(date: range)
-  #   @calendar = current_user.calendars
-  # end
 end
