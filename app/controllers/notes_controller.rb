beecc2ff6b5aaf481841af058251cc2363327e48
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-12-16 11:38:20 +0900

    連絡帳のメッセージの文字制限をフロンドで行う

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,9 +10,16 @@ class NotesController < ApplicationController
   end
 
   def create
-    current_user.notes.create!(note_params)
-    flash[:notice] = "投稿しました"
-    redirect_to root_path
+    # binding.pry
+    @note = Note.new(note_params)
+    if @note.save
+      current_user.notes.create!(note_params)
+      flash[:notice] = "投稿しました"
+      redirect_to root_path
+    else
+      flash[:alert] = "すでに本日の連絡帳が存在しています"
+      redirect_to root_path
+    end
   end
 
   def show
